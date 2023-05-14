ac7fa2e876ec06be016671ac410d6192b1588ce5
Steven Zhu <65844236+steveyxz@users.noreply.github.com>
2023-05-14 20:05:54 +1000

    Lists.implode()

--- core/src/me/partlysunny/gdxlib/util/Lists.java
+++ core/src/me/partlysunny/gdxlib/util/Lists.java
@@ -1,5 +1,6 @@
 package me.partlysunny.gdxlib.util;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,6 +9,15 @@ public class Lists {
     @SafeVarargs
     public static <T> List<T> of(T... items) {
         return Arrays.asList(items);
+    }
+
+    @SafeVarargs
+    public static <T> List<T> implode(List<T>... lists) {
+        List<T> result = new ArrayList<>();
+        for (List<T> list : lists) {
+            result.addAll(list);
+        }
+        return result;
     }
 
 }
